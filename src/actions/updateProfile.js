4ab60d1bee81faacd0252129e62e12a3fda72a62
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-04-03 12:49:18 -0300

    REMOVE: Deleting old project

--- src/actions/updateProfile.js
+++
@@ -1,6 +0,0 @@
-'use server'
-
-export default async function execute(state, formData){
-    console.log('update profile action called')
-}
-
--- src/app/components/ProfileLink.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProfileLink() {
-  return (
-    <>
-      <span className="text-ivor-400">@Fulano</span>
-    </>
-  );
-}
